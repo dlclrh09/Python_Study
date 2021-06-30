90b828d20ba4a3ae8b6f2ef15821d2d05d811645
dlclrh09 <85426611+dlclrh09@users.noreply.github.com>
2021-06-30 13:05:38 +0900

    Python basis

---
+++ Step_basis/practice/7_연산자.py
@@ -0,0 +1,28 @@
+print(1+1) #1
+print(3-2) #1
+
+print(2**3) #2^3 = 8
+
+print(5%3) # 5/3의 나머지 2
+print(10%3) # 10/3의 나머지 1
+
+print(5//3) # 5/3의 몫1
+print(10//3) # 10/3의 몫3
+
+print(10>3) #참 True
+print(4>=3) #거짓 False
+
+print(3==3) #앞과 뒤의 값이 값은지 / 참 True
+print(4==2) #거짓 False
+
+print(10!=3) #!= 앞뒤가 같지 않다 /참 True
+print(not(10!=3)) #not False
+
+print((3>0) and (3<5)) #True 둘 다 만족해야함
+print((3>0) & (3<5)) #True 둘 다 만족해야함
+
+print((3>0) or (3>5)) # 둘 중의 하나만 맞으면 됨 True
+print((3>0) | (3<5)) # 둘 중의 하나만 맞으면 됨 True
+
+print(5>4>3) #True
+print(5>4>7) #False
