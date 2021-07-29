0c042ef451afde00f58dee777479a0227cde27d9
dlclrh09 <dlclrh788@gmail.com>
2021-07-29 23:04:21 +0900

    Chapter 4 – Lists

--- Chapter 4 – Lists/List.py
+++ Chapter 4 – Lists/List.py
@@ -1,9 +1,18 @@
-#리스트는 순서를 가진 여러가지 값의 배열
-#[] 안에 들어가고 리스트 안에 있는 값들은 아이템이라고 함
+# 리스트는 순서를 가진 여러가지 값의 배열
+# [] 안에 들어가고 리스트 안에 있는 값들은 아이템이라고 함
 # 문자열은 '' 따옴표 안에 넣어서 표시
 
-[1,2,3]
-['cat', 'bat', 'rat', 'elephant']
-['hello', 3.1415, True, None, 42]
-spam=['cat', 'bat', 'rat', 'elephant']
-spam
+spam = ['cat', 'bat', 'bat', 'elephant']
+spam[0]
+'cat'
+spam[1]
+'bat'
+spam[2]
+'elephant'
+spam[3]
+spam=['cat', 'bat', 'bat', 'elephant'][3]
+'elephant'
+'Hello'+spam[0]
+'Hello cat'
+'The'+spam[1]+'ate the'+spam[0]
+'The bat are the cat'
